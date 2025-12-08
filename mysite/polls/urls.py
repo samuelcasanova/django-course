a639d8231b8fcaf03df55ad50cce15bed106ce43
samuel.casanova <samuel.casanova@immfly.com>
2025-12-08 12:59:37 +0100

    refactor the views and urls code to use generic views instead to reduce redundant code

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,13 +6,13 @@ from . import views
 # urls unambiguously, i.e. 'polls:index' in templates
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     path("api/<int:id>", views.api, name="api"),
     path("class", views.ClassBasedView.as_view(), name="class"),
     path("bounce", views.bounce, name="bounce"),
     path("templated/<str:param>", views.TemplatedView.as_view(),
          name="templated"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,19 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.utils.html import escape
-from django.views import View
+from django.views import View, generic
 from polls.models import Question, Choice
 from django.db.models import F
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 
-def index(request):
-    latest_question_list = Question.objects \
-                           .order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
 def api(request, id):
@@ -38,19 +40,14 @@ class TemplatedView(View):
         return render(request, 'templated.html', info)
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    # alternative, get_object_or_404:
-    # question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
