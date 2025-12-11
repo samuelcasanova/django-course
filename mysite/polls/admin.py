fc327bde85110189fe9ecb9380c76e6781910ccd
samuel.casanova <samuel.casanova@immfly.com>
2025-12-11 07:58:48 +0100

    modify admin form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['question_text', 'pub_date'] This just defines the list of fields and their order
+    fieldsets = [
+        ('Question', {'fields': ['question_text']}),
+        ('Date', {'fields': ['pub_date']}),
+    ]
+
+
 admin.site.register(Question)
 admin.site.register(Choice)
